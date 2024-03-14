9535b273c39e2fccb84d809dfd3a155decdbea5e
JayaramHarry <121944470+JayaramHarry@users.noreply.github.com>
2024-03-14 15:09:10 +0530

    published app
    published total code

---
+++ src/components/Home/index.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import "./style.css"
+
+const Home = () => {
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    fetch('https://v2.jokeapi.dev/joke/any?format=json&blacklistFlags=nsfw,sexist&type=single&lang=EN&amount=10')
+      .then(response => response.json())
+      .then(data => {
+        setJokes(data.jokes);
+      })
+      .catch(error => {
+        console.error('Error fetching jokes:', error);
+      });
+  }, []);
+
+  return (
+    <div className='jokes-container'>
+      <h1>Jokes</h1>
+      <table className="table table-dark table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Type</th>
+            <th scope="col">Joke</th>
+          </tr>
+        </thead>
+        <tbody>
+          {jokes.map((joke, index) => (
+            <tr key={index}>
+              <th scope="row">{index + 1}</th>
+              <td>{joke.category}</td>
+              <td>{joke.joke}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Login/index.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import {useNavigate} from 'react-router-dom'
+
+import "./style.css"
+
+const Login = () => {
+
+  const [username, setUsername] = useState('harry');
+  const [password, setPassword] = useState('harry123');
+  const [usernameError, setUsernameError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  
+  const navigate = useNavigate();
+
+  const dummyCredentials = {
+    username: 'harry',
+    password: 'harry123'
+  };
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username === dummyCredentials.username && password === dummyCredentials.password) {
+      navigate('/home');
+      
+    } else {
+      if (username !== dummyCredentials.username) {
+        setUsernameError('Incorrect username');
+      } else {
+        setUsernameError('');
+      }
+      if (password !== dummyCredentials.password) {
+        setPasswordError('Incorrect password');
+      } else {
+        setPasswordError('');
+      }
+    }
+  };
+
+  return (
+    <div className="container"> 
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div className="form-group">
+          <label>Username:</label>
+          <input type="text" className="form-control" value={username} onChange={handleUsernameChange} />
+          {usernameError && <div className="text-danger">{usernameError}</div>}
+        </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input type="password" className="form-control" value={password} onChange={handlePasswordChange} />
+          {passwordError && <div className="text-danger">{passwordError}</div>}
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+        <p>*The username and password are pre-filled. Click the login button to proceed.</p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
